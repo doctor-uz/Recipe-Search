c72407ff7e4332b285ee172da9407d4f65971585
Dilshod Rahmatov <dilshodrahmatov@gmail.com>
2019-01-17 20:44:30 +0100

    count again

--- src/App.js
+++ src/App.js
@@ -7,14 +7,18 @@ const secrets = require("./secrets.json");
 const API_KEY = secrets.KEY;
 
 class App extends Component {
+  state = {
+    recipes: []
+  };
+
   getRecipe = async e => {
-    const recipeName = e.target.elements.recipeName.value;
+    // const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
     const api_call = await fetch(
       `https://www.food2fork.com/api/search?key=${API_KEY}&q=chicken%20breast&page=5&count=10`
     );
     const data = await api_call.json();
-    console.log(data);
+    console.log(data.recipes[0].recipe_id);
   };
   render() {
     return (
