877d6e02724ee10221550cae1db26c35e006f4ec
Dilshod Rahmatov <dilshodrahmatov@gmail.com>
2019-01-17 20:26:31 +0100

    api key

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import React, { Component } from "react";
 import "./App.css";
 import Form from "./components/Form";
 
+const secrets = require("./secrets.json");
+
+const API_KEY = secrets.KEY;
+
 class App extends Component {
-  getRecipe = e => {
-    e.preventDefault();
+  getRecipe = async e => {
     const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault();
+    const api_call = await fetch(
+      `https://www.food2fork.com/api/search?key=API_KEY&q=chicken%20breast&page=5`
+    );
     console.log(recipeName);
   };
   render() {
