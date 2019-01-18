80407ca145c31bce9a45e4bd56aacf052876b02f
Dilshod Rahmatov <dilshodrahmatov@gmail.com>
2019-01-18 16:15:43 +0100

    styling

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,15 +1,25 @@
 import React from "react";
 
 const Recipes = props => (
-  <div>
-    {props.recipes.map(recipe => {
-      return (
-        <div key={recipe.recipe_id}>
-          <img src={recipe.image_url} alt={recipe.title} />
-          <p>{recipe.title} </p>
-        </div>
-      );
-    })}
+  <div className="container" key={props.recipe_id}>
+    <div className="row">
+      {props.recipes.map(recipe => {
+        return (
+          <div className="col-md-4">
+            <div className="recipe__box">
+              <img
+                className="recipe__box-img"
+                src={recipe.image_url}
+                alt={recipe.title}
+              />
+              <div className="recipe__text">
+                <h5>{recipe.title}</h5>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
   </div>
 );
 
