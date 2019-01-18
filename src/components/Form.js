e10af913d54ccf9d373c62622159c1dfad0e878f
Dilshod Rahmatov <dilshodrahmatov@gmail.com>
2019-01-18 16:01:11 +0100

    styling ...

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Form = props => (
-  <form onSubmit={props.getRecipe}>
+  <form onSubmit={props.getRecipe} style={{ marginBottom: "2rem" }}>
     <input type="text" name="recipeName" />
     <button>Search</button>
   </form>
