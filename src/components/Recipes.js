7593f4aeb79f95477a7932cbbdeec5ba4345c308
Dilshod Rahmatov <dilshodrahmatov@gmail.com>
2019-01-18 17:08:39 +0100

    style img css

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -10,16 +10,23 @@ const Recipes = props => (
       {props.recipes.map(recipe => {
         return (
           <div className="col-md-4">
-            <div className="recipe__box">
+            <div className="recipes__box">
               <img
                 className="recipe__box-img"
                 src={recipe.image_url}
                 alt={recipe.title}
               />
               <div className="recipe__text">
-                <h5 className="recipes__title">{recipe.title}</h5>
-                <p className="recipes__subtitle" />
+                <h5 className="recipes__title">
+                  {recipe.title.length < 20
+                    ? `${recipe.title}`
+                    : `${recipe.title.substring(0, 25)}...`}
+                </h5>
+                <p className="recipes__subtitle">
+                  Publisher: <span>{recipe.publisher}</span>
+                </p>
               </div>
+              <button className="recipe_buttons">View Recipe</button>
             </div>
           </div>
         );
